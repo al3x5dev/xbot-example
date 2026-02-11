d96cc8e8d2d858e1376d422106b7cfb9e0829492
al3x5dev <al3x5.dev@gmail.com>
2026-02-11 00:42:05 -0500

    add: Help command to display the list of commands

---
+++ bot/Commands/Help.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bot\Commands;
+
+use Al3x5\xBot\Commands;
+
+use Al3x5\xBot\Attributes\Command;
+use Al3x5\xBot\Events;
+use Al3x5\xBot\FormatHelper;
+
+#[Command('/help')]
+class Help extends Commands
+{
+    public function execute(): void
+    {
+        $message = '';
+
+        // getCommandsList() returns an array where:
+        // key   => command name
+        // value => command description
+        foreach ($this->getCommandsList() as $key => $value) {
+            $message .= "$key - $value\n";
+        }
+
+        $this->reply(
+            sprintf(
+                "%s\n%s",
+                // Text formatting using FormatHelper::class
+                FormatHelper::bold('Commands List'),
+                FormatHelper::expandableBlockQuote($message)
+            )
+        );
+    }
+
+    public static function description(): string
+    {
+        return 'Show bot commands help';
+    }
+}
