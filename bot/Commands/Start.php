08a161fedb959d4ef9cdebba750d33d20d8c79b4
al3x5dev <al3x5.dev@gmail.com>
2026-02-11 00:40:27 -0500

    add: start command

---
+++ bot/Commands/Start.php
@@ -0,0 +1,21 @@
+<?php
+namespace Bot\Commands;
+
+use Al3x5\xBot\Commands;
+
+use Al3x5\xBot\Attributes\Command;
+
+#[Command('/start')]
+class Start extends Commands
+{
+    public function execute(): void
+    {
+        $name = $this->message->from->first_name;
+        $this->reply("👋 Hi $name, run /help to see everything i can do.");
+    }
+    
+    public static function description(): string
+    {
+        return 'Start command';
+    }
+}
