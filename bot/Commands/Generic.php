1347aaee0056b208c4c89179057b749a8f0201ac
al3x5dev <al3x5.dev@gmail.com>
2026-02-11 00:43:27 -0500

    add: execution of other commands

---
+++ bot/Commands/Generic.php
@@ -0,0 +1,21 @@
+<?php
+namespace Bot\Commands;
+
+use Al3x5\xBot\Commands;
+
+use Al3x5\xBot\Attributes\Command;
+
+#[Command('/generic')]
+class Generic extends Commands
+{
+    public function execute(): void
+    {
+        // Forces the execution of the /help command
+        $this->executeCommand('/help');
+    }
+    
+    public static function description(): string
+    {
+        return 'Generic command handler or text messages';
+    }
+}
