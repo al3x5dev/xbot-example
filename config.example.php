16d3c9aee95756fe5cff765129cb0481f0a553d4
al3x5dev <al3x5.dev@gmail.com>
2026-05-19 20:32:31 -0400

    add example config

--- config.example.php
+++ config.example.php
@@ -12,14 +12,18 @@
  */
 
 return [
-    // your bot token
+    // your bot token (required)
     'token' => '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11',
+    // webhook secret token (optional but recommended)
+    // This adds an extra layer of security to your webhook
+    // Telegram will send this token in the X-Telegram-Bot-Api-Secret-Token header
+    //'secret' => 'your_secret_token_here',
     // list of bot admin id's
-    'admins' => [123456789, 985632147],
+    //'admins' => [123456789, 985632147],
     // http client to use (By default \Mk4U\Http\Client is used)
-    #'http_client' => new \Mk4U\Http\Client(),
+    //'http_client' => new \Mk4U\Http\Client(),
     // cache handling library used (default is mk4u/cache)
-    #'cache' => \Mk4U\Cache\CacheFactory::create('file', ['dir' => 'storage', 'ttl' => 300]),
+    //'cache' => \Mk4U\Cache\CacheFactory::create('file', ['dir' => 'storage', 'ttl' => 300]),
     // activate dev environment (default is false)
     'debug' => true,
     // sets the base path of the library
